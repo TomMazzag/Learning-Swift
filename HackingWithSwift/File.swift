df4d13f922fc0f1c9f2e0c82f8e70cc82da1e907
TomMazzag <itstomazz@gmail.com>
2024-01-24 15:42:35 +0000

    Initial Commit

---
+++ HackingWithSwift/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tom Mazzag on 24/01/2024.
+//
+
+var name = "Tom"
+
+let user = "PrimaryUser"
---
+++ HackingWithSwift/LearningSwift.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Tom"
+
+let user = "PrimaryUser"
+
+print(name)
+print(user)
+
+let sentance = "This is a string"
+let quote = "\"This is a quotue\""
+print(quote)
+
+let multiLineSting = """
+This is a
+multi line
+String
+"""
+
+print(sentance.count)
